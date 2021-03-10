f2f37d860dfc5c7371403fd24679162de39ae11f
Ogilvyr9201 <78511761+Ogilvyr9201@users.noreply.github.com>
2021-03-10 13:20:27 +1300

    Base component
    Added comments

---
+++ 01_rps_base_component.py
@@ -0,0 +1,26 @@
+import random
+
+# Functions
+
+
+
+# Main Routines
+
+
+# Vaild response lists
+yes_no_list = ["yes", "no"]
+rps_list = ["rock", "paper", "scissors", "xxx"]
+
+
+# ask user if they have played before.
+# If 'no' show instructions
+
+
+# Ask user for # of rounds then loop...
+
+
+
+# Ask user if they want to see there game history
+# If 'yes' show game history
+
+
